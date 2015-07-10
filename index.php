5d41c707ddefc8f6b7c92a3592156788fdb3b602
sousa47 <davimfs7@gmail.com>
2015-07-10 19:51:40 +0100

    adiciona css butão

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8"> 	<!--Permite usar caracteres latinos como os acentos-->
+	<link rel="stylesheet" type="text/css" href="styles.css">			<!--Mete as styles-->
+</head>
+
+<body>
+
+<!--Step 1: The HTML-->
+<a href="http://designshack.net/" class="button">Click Me</a>
+
+<?php
+//echo "Olá Mundo!";
+?>
+
+</body>
+</html>
