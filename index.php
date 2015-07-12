312cd5671c14e5e2cf5f1824f652e5d12eea9859
sousa47 <davimfs7@gmail.com>
2015-07-12 17:29:35 +0100

    animações

--- index.php
+++ index.php
@@ -17,6 +17,10 @@
 </br>
 
 <a href="#" class="dropbox">+</a>
+</br>
+<a href="#" class="dropbox">+</a>
+</br>
+<div class="dropbox">Teste</div>
 
 <?php
 //echo "Olá Mundo!";
@@ -27,6 +31,8 @@
 $(document).ready(function(){
 	var msg = false;
 	var msg2 = false;
+	var slide = false;
+	var anim = true;
 	
    $(".button").dblclick(function(){
 	   //http://www.w3schools.com/jquery/jquery_selectors.asp
@@ -81,21 +87,82 @@ $(document).ready(function(){
 	
 	//The on() Method
 	//The on() method attaches one or more event handlers for the selected elements.
-	$(".dropbox").on("click", function(){
-		//alert("Clicado!"); //spam...
-		//$(".button").toggle();
-		//http://www.w3schools.com/jquery/jquery_fade.asp
-		$(".button").fadeOut(2000);//desaparece
-		$(".button").fadeIn(2000);//aparece
-		$(".button").fadeToggle("fast");
-		$(".button").fadeToggle("slow");
-		$(".button").fadeTo(10000, 0.70);
-		//$("#div2").fadeIn("slow");
-		//$("#div3").fadeIn(3000);
+	if(msg2)//porque posso
+	{
+		$(".dropbox").on("click", function(){
+			//alert("Clicado!"); //spam...
+			//$(".button").toggle();
+			//http://www.w3schools.com/jquery/jquery_fade.asp
+			$(".button").fadeOut(2000);//desaparece
+			$(".button").fadeIn(2000);//aparece
+			$(".button").fadeToggle("fast");
+			$(".button").fadeToggle("slow");
+			$(".button").fadeTo(10000, 0.70);
+			//$("#div2").fadeIn("slow");
+			//$("#div3").fadeIn(3000);
 
-	});
+		});
+	}
+
+	if(slide)
+	{
+		//Slides
+		
+		//slideDown() Method
+		$(".dropbox").click(function(){ //precissa de ficar invisivel com o hide ou com 
+										//css display: none; de forma a aparecer do "nada"
+			 //ups
+			 //http://stackoverflow.com/questions/31368704/selecting-the-element-with-this
+			 $(this).hide().slideDown();
+			 //site interessante de seletor
+			 //http://www.javascript-coder.com/jquery/jquery-selector-this.phtml
+			 //nunca se sabe
+			 //http://learn.jquery.com/using-jquery-core/
+			 //alert("oi");
+			 
+			 //slideUp()
+			 $("div.dropbox").slideUp();
+			 
+			 //slideToggle() 
+			 $("div.dropbox").slideToggle();
+			 $("div.dropbox").slideToggle();
+			 $("div.dropbox").slideToggle();
+			 $("div.dropbox").slideToggle();
+			 $("div.dropbox").slideToggle();
+		});
+	}
 	
+	if(anim)
+	{
+		//Animate
+		$(".dropbox").click(function(){
+			$(this).css("position","relative");
+			$(this).animate({left: '250px'});
+			/*
+				Is it possible to manipulate ALL CSS properties 
+				with the animate() method?
 
+				Yes, almost! However, there is one important thing 
+				to remember: all property names must be camel-cased
+				when used with the animate() method: You will need 
+				to write paddingLeft instead of padding-left, marginRight 
+				instead of margin-right, and so on. 
+
+				Also, color animation is not included in the core jQuery library.
+				If you want to animate color, you need to download 
+				the Color Animations plugin from jQuery.com.
+			*/
+			$(this).animate({
+				width: '500px',//toggle ou hide para o slide de lado :)
+				height: '80px' //toggle, (especie de 0px) / show e hide, mesmo que a animação slide 
+				//no entanto o border-radius nao da nem o background :/
+			});
+			
+			//o w3 diz que isto faz uma queque. ^^^^^^ (ou seja so executa um apos o outro)
+			
+		});
+	}
+	
 });
 </script>
 </html>
